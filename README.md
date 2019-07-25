92b62d060189756abca704dfd42e269da3b4763e
pengdaokuan <1063137960@qq.com>
2019-07-25 18:03:13 +0800

    feat: 新增弹窗类型

--- README.md
+++ README.md
@@ -5,95 +5,159 @@
 ![](https://img.shields.io/badge/Author-PDK-yellow.svg)
 
 ## Explain
+
 1 . Confirm Dialog Modal
 
 2 . Support customization，for example: color 、 position 、 text ...
 
-3 . .....(There will be more follow ups)
+3 . `normal confirm dialog` and `input confirm dialog`
 
-------
+4 . .....(There will be more follow ups)
+
+---
 
 ## Demo
-<img src='./image/img_1.png' width='250'>
 
---------
+<img src='https://github.com/PDKSophia/p-dialog-modal/raw/master/image/image1.png' width='250'>
+
+<img src='https://github.com/PDKSophia/p-dialog-modal/raw/master/image/image2.png' width='250'>
+
+---
+
 ## Usage
+
 ### 1.1 Installation
+
 ```javascript
   npm install p-dialog-modal --save
 ```
+
 ### 1.2 ES6 Import
+
 ```javascript
-  import ConfirmDialog from 'p-dialog-modal'
-  
-  export default {
-    components: {
-      ConfirmDialog
-    }
+import ConfirmModal from 'p-dialog-modal'
+
+export default {
+  components: {
+    ConfirmModal
   }
+}
 ```
 
 ## Basic Example
+
 html
+
 ```html
-  <template>
-    <div>
-      <confirm-dialog 
-        :confrimMesasge=confrimMesasge 
-        @onHandeleCancle='handleOnCancle' 
-        @onHandeleOnOK='handleOnOk'
-      />
-    </div>
-  </template>
+<template>
+  <div>
+    <confirm-modal
+      :type="confirmModal.type"
+      :title="confirmModal.title"
+      :content="confirmModal.content"
+      :cancelText="confirmModal.cancelText"
+      :submitText="confirmModal.submitText"
+      :cssStyle="confirmModal.cssStyle"
+      :noneToastText="confirmModal.noneToastText"
+      :isMarginTop="confirmModal.isMarginTop"
+      @onHandleCancel="handleOnCancel"
+      @onHandleOnOk="handleOnOk"
+    />
+  </div>
+</template>
 ```
 
 js
-```javascript
-  import ConfirmDialog from 'p-dialog-modal'
 
-  export default {
-    components: {
-      ConfirmDialog
-    },
-    data () {
-      return {
-        confrimMesasge: {
-          title: 'i am title',
-          text: 'i am content',
-          headerColor: '#2894FF',
-          okColor: 'red',
-          cancelText: 'cancel',
-          okText: 'ok',
-          marginTop: '300px'
+```javascript
+import ConfirmModal from 'p-dialog-modal'
+
+export default {
+  components: {
+    ConfirmModal
+  },
+  data() {
+    return {
+      confirmModal: {
+        type: 'input',
+        title: '请输入用户名',
+        content: '你要给我点个赞嘛 ?',
+        cancelText: '取消',
+        submitText: '确认',
+        cssStyle: {
+          bgTitleColor: '#4d4d4d',
+          textTitleColor: '#ffffff',
+          bgCancelColor: '#fff',
+          textCancelColor: '#555',
+          bgSubmitColor: '#4d4d4d',
+          textSubmitColor: '#fff'
         },
+        isMarginTop: '35%'
       }
     }
+  },
+  methods: {
+    // submit
+    handleOnOk(type, bool, value) {
+      if (type === 'normal') {
+        console.log(type, bool, value) // normal  true  ''
+      } else {
+        console.log(type, bool, value) // normal  true  value
+      }
+    },
+    // cancel
+    handleOnCancel(type, bool) {
+      console.log('?????', type, bool)
+    }
   }
-
+}
 ```
 
 ### Props
-props | type | default | description
----|---|---|---
-title | String |  xxx提示您？ | This is the confrime title
-text | String | 我帅吗 ? | This is the confrime content
-headerColor | String | #4d4d4d | header title background color 
-okColor | String | #4d4d4d | ok button background color
-cancelText | String | 不帅 | the cancel text
-onText | String | 帅 | the ok text
-marginTop | String | 30% | margin top
+
+|    props    |  type  |      default       |                                    description                                     |
+| :---------: | :----: | :----------------: | :--------------------------------------------------------------------------------: |
+|    type     | String |       normal       | `normal` represents a regular modal，`input` represents a regular modal with input |
+|    title    | String |     弹窗提示您     |                                Confirm Modal Title                                 |
+|   content   | String | 你要给我点个赞嘛 ? |                               Confirm Modal Content                                |
+| cancelText  | String |        取消        |                                 Cancel Button Text                                 |
+| submitText  | String |        确认        |                                 Submit Button Text                                 |
+|  cssStyle   | Object |         {}         |                                Confirm Modal Style                                 |
+| isMarginTop | String |        35%         |                 The distance between the elastic layer and the top                 |
+
+---
+
+`Here is the props of **cssStyle**`
+
+|      props      |  type  | default |          description           |
+| :-------------: | :----: | :-----: | :----------------------------: |
+|  bgTitleColor   | String | #ffffff |     title background color     |
+| textTitleColor  | String | #4a4a4a |        title text color        |
+|  bgCancelColor  | String | #ffffff | Cancel button background color |
+| textCancelColor | String |  #555   |    Cancel button text color    |
+|  bgSubmitColor  | String | #4d4d4d | Submit button background color |
+| textSubmitColor | String |  #fff   |    Submit button text color    |
 
 ### Event
-When you press the confirm button，return true， tigger handleOnOk(data)
-```javascript
-    handleOnOk (data) {
-      console.log(data) // true
-    }
 
-```
-When you press the cancel button，return false， tigger handleOnCancle(data)
+When you press the confirm modal **cancel** button，trigger `handleOnCancel()`
+
 ```javascript
-    handleOnCancle (data) {
-      console.log(data) // false
+    handleOnCancel(type, bool) {
+      console.log('close modal', type, bool)
+    }
+```
+
+When you press the confirm modal **submit** button，trigger `handleOnOk(type, value)`
+
+```javascript
+    handleOnOk(type, value) {
+      if (type === 'normal') {
+        console.log('OK')
+        console.log('close modal')
+      } else {
+        console.log('callback value is : ', value)
+        console.log('close modal')
+      }
     }
 ```
--- src/app.vue
+++ src/app.vue
@@ -1,173 +1,282 @@
 <template>
-  <div class='mask'>
-    <div class='toastbg'></div>
-      <div class='modal-dialog' :style='[{marginTop: confrimMesasge.marginTop ? confrimMesasge.marginTop : "50%" }]'>
-        <div class='modal-title' :style='[{backgroundColor: confrimMesasge.headerColor ? confrimMesasge.headerColor : "#4d4d4d;" }]'>{{ confrimMesasge.title }}</div>
-        <div class='modal-input'>
-            <p>{{ confrimMesasge.text }}</p>
-          </div>
-          <div class='modal-button'>
-            <div class='main-button btn-cancel'
-                @click='handleCancle'
-            > {{ confrimMesasge.cancelText }}
-            </div>
-            <div class='main-button btn-submit'
-                :style='[{backgroundColor: confrimMesasge.okColor ? confrimMesasge.okColor : "#4d4d4d;" }]'
-                @click='handleOnOk'
-            > {{ confrimMesasge.okText }}
-            </div>
-          </div>
+  <div class="modal-mask" @click="handleMaskClose">
+    <div class="modal-toastbg">
+      <div
+        class="modal-content pt-page-scaleBounce"
+        :style="{ top: isMarginTop  }"
+        @click="handleStopPropagation"
+      >
+        <div
+          class="modal-title"
+          :style="{
+            backgroundColor: cssStyle.bgTitleColor,
+            color: cssStyle.textTitleColor
+          }"
+        >
+          <span>{{ title }}</span>
+        </div>
+        <div class="content-container" v-if="type === 'normal'">
+          {{
+          content
+          }}
+        </div>
+        <div class="textarea-container" v-else>
+          <textarea
+            class="content-box"
+            placeholder="输入些啥..."
+            v-model="textareaValue"
+            :style="{ height: cssStyle.contentHeight }"
+          />
+        </div>
+        <div class="modal-button">
+          <div
+            class="main-button btn-cancel"
+            @click="hangleClickCancel"
+            :style="{
+              backgroundColor: cssStyle.bgCancelColor,
+              color: cssStyle.textCancelColor
+            }"
+          >{{ cancelText }}</div>
+          <div
+            class="main-button btn-submit"
+            @click="handleClickSubmit"
+            :style="{
+              backgroundColor: cssStyle.bgSubmitColor,
+              color: cssStyle.textSubmitColor
+            }"
+          >{{ submitText }}</div>
         </div>
       </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'ConfirmModal',
+
+  data() {
+    return {
+      textareaValue: '',
+      showToastModal: false
+    };
+  },
   props: {
-    confrimMesasge: {
+    title: {
+      type: String,
+      default: '弹窗提示您'
+    },
+    content: {
+      type: String,
+      default: '你要给我点个赞嘛 ?'
+    },
+    type: {
+      type: String,
+      default: 'normal'
+    },
+    cancelText: {
+      type: String,
+      default: '取消'
+    },
+    disableCloseMask: {
+      type: Boolean,
+      default: true
+    },
+    submitText: {
+      type: String,
+      default: '确定'
+    },
+    cssStyle: {
       type: Object,
       default: function () {
         return {
-          title: 'xxx提示您？',
-          text: '我帅吗 ?',
-          cancelText: '不帅',
-          okText: '帅',
-          headerColor: '',
-          okColor: '',
-          marginTop: '50%'
-        }
+          bgTitleColor: 'white',
+          textTitleColor: '#4a4a4a',
+          bgCancelColor: 'white',
+          textCancelColor: '#555',
+          bgSubmitColor: '#4d4d4d',
+          textSubmitColor: '#fff'
+        };
       }
+    },
+    isMarginTop: {
+      type: String,
+      default: '35%'
     }
   },
   methods: {
-    handleCancle () {
-      console.log('cancle')
-      this.$emit('onHandeleCancle', false)
+    handleMaskClose() {
+      if (this.disableCloseMask) {
+        console.log('not allowed click close !');
+      } else {
+        this.$emit('onHandleCancel', false);
+      }
     },
-    handleOnOk () {
-      console.log('ok')
-      this.$emit('onHandeleOnOK', true)
+
+    handleStopPropagation(e) {
+      e.stopPropagation();
+    },
+    hangleClickCancel() {
+      this.$emit('onHandleCancel', this.type, false)
+    },
+    handleClickSubmit() {
+      if (this.type === 'normal') {
+        this.$emit('onHandleOnOk', 'normal', true, '');
+      } else {
+        this.$emit('onHandleOnOk', 'input', true, this.textareaValue);
+      }
     }
-  },
-  mounted () {
   }
-}
+};
 </script>
 
-<style scoped lang='less'>
-.mask {
+<style scoped lang="less">
+.modal-mask {
   width: 100%;
   height: 100%;
   opacity: 1;
   position: fixed;
   top: 0px;
   left: 0px;
+  z-index: 10;
 
-  > .toastbg {
-    opacity: 0.2;
-    background-color: black;
+  .modal-toastbg {
+    background-color: rgba(0, 0, 0, 0.4);
     position: absolute;
     width: 100%;
     min-height: 100%;
-  }
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  > .modal-dialog {
-    animation: bounceIn 1s;
-    position: absolute;
-    opacity: 1;
-    width: 70%;
-    margin-left: 15%;
-    margin-top: 70%;
-    background-color: white;
+    .modal-content {
+      position: absolute;
+      opacity: 1;
+      width: 80%;
+      background-color: white;
+      border-radius: 10px;
 
-    > .modal-title {
-      text-align: center;
-      padding: 15px;
-      font-size: 15px;
-      color: white;
-      background-color: #4d4d4d;
-    }
-
-    > .modal-input {
-      padding: 15px 20px;
-      font-size: 16px;
-      color: #4d4d4d;
-      text-align: center;
-    }
-
-    > .modal-button {
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-content: center;
-
-      > .main-button {
-        width: 50%;
-        padding: 10px;
-        font-size: 13px;
+      .modal-title {
+        font-family: PingFangSC-Semibold;
+        font-size: 17px;
+        color: #4a4a4a;
+        letter-spacing: 1.4px;
         text-align: center;
+        padding: 14px 0;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+      }
 
-        > Button {
-          width: 100%;
+      .modal-button {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 42px;
+        text-align: center;
+        border-top: 2px solid #f1f1f1;
+
+        > .main-button {
+          width: 50%;
+          padding: 10px;
+          font-size: 14px;
+          height: 22px;
+          line-height: 22px;
+          text-align: center;
+        }
+
+        > .btn-cancel {
+          background-color: #f5f5f5;
+          color: #555;
+          border-bottom-left-radius: 10px;
+        }
+
+        > .btn-submit {
+          background-color: #4d4d4d;
+          color: #fff;
+          border-bottom-right-radius: 10px;
         }
       }
-
-      > .btn-cancel {
-        background-color: #f5f5f5;
-        color: #555;
-      }
-
-      > .btn-submit {
-        background-color:#4d4d4d;
-        color: #fff;
-      }
     }
   }
 }
-.placeholder{
-  font-size: 15px;
+.content-container {
+  width: 90%;
+  padding: 15px 5%;
+  line-height: 26px;
   color: #555;
-}
-input{
-  color: #555 !important;
-  font-size: 15px;
+  font-size: 18px;
   text-align: center;
+  border-top: 1px solid #f5f5f5;
 }
-@keyframes bounceIn {
-  from,
-  20%,
-  40%,
-  60%,
-  80%,
-  to {
-    animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-  }
+.textarea-container {
+  width: 90%;
+  padding: 5%;
 
+  > .content-box {
+    width: 100%;
+    line-height: 22px;
+    font-size: 15px;
+    height: 110px;
+    outline: none;
+    padding: 5px;
+    box-sizing: border-box;
+  }
+}
+.pt-page-scaleBounce {
+  -webkit-animation: scaleBounceLeftIn 0.5s ease-in both;
+  -moz-animation: scaleBounceLeftIn 0.5s ease-in both;
+  animation: scaleBounceLeftIn 0.5s ease-in both;
+}
+@-webkit-keyframes scaleBounceLeftIn {
   0% {
-    opacity: 0;
-    transform: scale3d(0.3, 0.3, 0.3);
+    -webkit-transform: scale(0.4);
   }
-
-  20% {
-    transform: scale3d(1.1, 1.1, 1.1);
+  25% {
+    -webkit-transform: scale(0.7);
   }
-
-  40% {
-    transform: scale3d(0.9, 0.9, 0.9);
+  50% {
+    -webkit-transform: scale(1.2);
   }
-
-  60% {
-    opacity: 1;
-    transform: scale3d(1.03, 1.03, 1.03);
+  75% {
+    -webkit-transform: scale(0.8);
   }
-
-  80% {
-    transform: scale3d(0.97, 0.97, 0.97);
+  100% {
+    -webkit-transform: scale(1);
   }
-
-  to {
-    opacity: 1;
-    transform: scale3d(1, 1, 1);
+}
+@-moz-keyframes scaleBounceLeftIn {
+  0% {
+    -moz-transform: scale(0.4);
+  }
+  25% {
+    -moz-transform: scale(0.7);
+  }
+  50% {
+    -moz-transform: scale(1.2);
+  }
+  75% {
+    -moz-transform: scale(0.8);
+  }
+  100% {
+    -moz-transform: scale(1);
+  }
+}
+@keyframes scaleBounceLeftIn {
+  0% {
+    transform: scale(0.4);
+  }
+  25% {
+    transform: scale(0.7);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  75% {
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
   }
 }
 </style>
